98ba94aa3632f0d092c2e44ef98cfb3662fee735
Carlos RR <carlos.facundo.rr@gmail.com>
2024-06-19 09:38:28 -0300

    Mensaje

--- src/app/(components)/BotonesFlotantes.jsx
+++ src/app/(components)/BotonesFlotantes.jsx
@@ -15,7 +15,7 @@ export const BotonesFlotantes = () => {
    
         <div className={`${pathname == '/ubicacion' || pathname == '/login' ? 'hidden' : ''} fixed right-0 bottom-[110px] md:bottom-0 z-50  mr-3 xl:mb-8 xl:mr-6  `}>
 
-            <div className={`${pathname == '/radio' ? swiper == 'Chat' ? 'relative bottom-[150px] md:mb-0' : 'hidden' : ''}`}>
+            <div className={`${pathname == '/radio' ? swiper == 'Chat' ? 'relative bottom-[150px] md:bottom-0 md:mb-0' : 'hidden' : ''}`}>
                 <Biblia />
             </div>
 
